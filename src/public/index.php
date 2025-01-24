0db6c182d7c9b9b7c9c55ff226ec2a3e3418400e
mwojcikbs <mwojcikbs@gmail.com>
2025-01-24 10:08:47 +0100

    add routing

--- src/public/index.php
+++ src/public/index.php
@@ -1,8 +1,18 @@
 <?php
 
-use App\Controllers\HomeController;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$controller = new HomeController();
-$controller->index();
+use App\Controller\HomeController;
+
+$uri = trim($_SERVER['REQUEST_URI'], '/');
+
+if ($uri === '' || $uri === 'home') {
+	$controller = new HomeController();
+	$controller->index();
+} elseif ($uri === 'generate' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+	$controller = new HomeController();
+	$controller->generate();
+} else {
+	http_response_code(404);
+	echo "Page not found.";
+}
